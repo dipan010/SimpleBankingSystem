914f040616b37620ada9583daf4db11e996681b4
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-03-16 21:31:19 +0530

    registered db

--- SimpleBankingSystem/settings.py
+++ SimpleBankingSystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'banktransfers'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'SimpleBankingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'transactiondb',  
+        'USER':'root',  
+        'PASSWORD':'QL2020db',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
     }
 }
 
