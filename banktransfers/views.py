659bd325f7c028e9e6535e9f3aaba671f009ae85
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-03-17 07:01:51 +0530

    changed routes

--- banktransfers/views.py
+++ banktransfers/views.py
@@ -4,14 +4,14 @@ from .forms import TransactionForm,BalanceForm
 # Create your views here.
 
 def index(request):
-    return render(request,'banktransfers/index.html')
+    return render(request,'index.html')
 
 def customers(request):
     customers = Customer.objects.all().order_by('id')
-    return render(request,'banktransfers/customers.html',context={'customers':customers})
+    return render(request,'customers.html',context={'customers':customers})
     
 
 def transactions(request):
     transactions = Transactions.objects.all().order_by('-id')
-    return render(request,'banktransfers/transactions.html',context={'transactions':transactions})
+    return render(request,'transactions.html',context={'transactions':transactions})
 
