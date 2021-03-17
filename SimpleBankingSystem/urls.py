b19d63ba7d90fe2bfea046ecd4ca89a28191f1e5
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-03-17 17:26:35 +0530

    added transfer routes

--- SimpleBankingSystem/urls.py
+++ SimpleBankingSystem/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from banktransfers import views
 
 urlpatterns = [
-     path('',views.index,name='index'),
-     path('customers',views.customers,name='customers'),
-     path('transactions',views.transactions,name='transactions'),
+    path('',views.index,name='index'),
+    path('customers',views.customers,name='customers'),
+    path('transactions',views.transactions,name='transactions'),
+    path('transfer/<int:cust_id>',views.transfer,name='transfer'),
 ]
