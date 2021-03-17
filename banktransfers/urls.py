e7b941bc6a0d1821995ff28dba52bb86822bc2f1
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-03-17 07:00:47 +0530

    deleted simplebankingsystem/urls

--- banktransfers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-     path('',views.index,name='index'),
-     path('customers',views.customers,name='customers'),
-     path('transactions',views.transactions,name='transactions'),
-]
