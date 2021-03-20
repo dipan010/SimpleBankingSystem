97c820d1616f936f941a8baf4993663396c10e7d
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-03-20 06:45:59 +0530

    shifted db to mysql

--- SimpleBankingSystem/settings.py
+++ SimpleBankingSystem/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'SimpleBankingSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'transactiondb',  
+        'USER':'root',  
+        'PASSWORD':'QL2020db',  
+        'HOST':'localhost',  
+        'PORT':'3306'  
     }
 }
 
