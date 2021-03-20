bc0dee39979667eec04a4a9b454418b01363c8c3
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-03-20 06:14:34 +0530

    commented

--- SimpleBankingSystem/settings.py
+++ SimpleBankingSystem/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 
